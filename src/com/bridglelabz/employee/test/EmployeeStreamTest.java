f91223915a63fc438673d263d78fc1acda2093e1
Darpan Madlani <Darpan@proclink.com>
2022-04-12 20:54:40 +0530

    Int element to Double with stream.map()

--- src/com/bridglelabz/employee/test/EmployeeStreamTest.java
+++ src/com/bridglelabz/employee/test/EmployeeStreamTest.java
@@ -1,6 +1,9 @@
 package com.bridglelabz.employee.test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -10,13 +13,14 @@ import com.bridglelabz.employee.EmployeeData;
 
 public class EmployeeStreamTest {
 
-	@Test
+//	@Test
 	public void streamForEach() {
 
 		EmployeeData empData = new EmployeeData();
-		
-		Stream<Employee> empStream = empData.empList.stream();
-		
+
+		Stream<Employee> empStream = empData.empList
+				.stream();
+
 		Consumer<Employee> empConsumer = e -> {
 			System.out.println(
 					"--------------------------------");
@@ -27,8 +31,27 @@ public class EmployeeStreamTest {
 			System.out.println("Salary : $" + e.getSalary()
 					+ " USD per annum");
 		};
-		
+
 		empStream.forEach(empConsumer);
+	}
+
+	@Test
+	public void streamTransform() {
+
+		List<Integer> intList = new LinkedList<>();
+
+		for (int i = 0; i < 5; i++) {
+			int num = (int) (Math.random() * 90 + 10);
+			intList.add(num);
+		}
+
+		Stream<Integer> intStream = intList.stream();
+
+		Function<Integer, Double> doubleMap = n -> n
+				.doubleValue();
+
+		intStream.map(doubleMap)
+				.forEach(n -> System.out.println(n));
 	}
 
 }
